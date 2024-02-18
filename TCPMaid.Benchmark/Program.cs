8085190b0dbe80cea248ddd157f2c31b0144e795
Joyless <65855333+Joy-less@users.noreply.github.com>
2024-02-18 17:18:00 +0000

    Add benchmark

---
+++ TCPMaid.Benchmark/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace TCPMaid.Benchmark {
+    internal class Program {
+        static void Main() {
+            // Initialise server
+            TCPMaidServer Server = new(12345);
+            Server.OnConnect += (Connection) => {
+                Console.WriteLine($"{Server.ClientCount} clients.");
+            };
+            Server.Start();
+
+            // Broadcast messages
+            Task.Run(async () => {
+                while (true) {
+                    const double BroadcastsPerSecond = 20;
+                    await Task.Delay(TimeSpan.FromSeconds(1.0 / BroadcastsPerSecond));
+                    await Server.BroadcastAsync(new BlankMessage());
+                }
+            });
+
+            // Connect clients
+            while (true) {
+                TCPMaidClient Client = new();
+                Client.OnConnect += (Connection) => {
+                    Console.WriteLine("Connected!");
+                };
+                Client.ConnectAsync("localhost", 12345).Wait();
+            }
+        }
+        class BlankMessage : Message { }
+    }
+}
